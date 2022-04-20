5ce2c6300d17e0da3585ff67cfd23841860fdf3e
Mrichk0 <zaq4769@gmail.com>
2022-04-21 01:05:14 +0300

    hw-7

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+
+const pictureGallery = document.querySelector(".gallery");
+const pictureMarkup = createPictureMarkup(galleryItems);
+
+pictureGallery.insertAdjacentHTML("beforeend", pictureMarkup);
+
+function createPictureMarkup(picture) {
+  return picture
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    )
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
